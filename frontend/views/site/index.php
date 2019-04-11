671be151c3c3d74a16fa03ab17c5894beb2c19ca
vlad <newsperparser@gmail.com>
2019-04-11 16:37:44 +1000

    верстка главной страницы

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -5,49 +5,56 @@
 $this->title = 'Билетур';
 ?>
 <div class="row">
-    <div class="col-xs-12">
+    <div class="col-xs-12 text-center">
+        <div class="banner-hr-block">
+
+        </div>
+    </div>
+</div>
+<div class="row" style="margin-top: 20px;">
+    <div class="col-xs-12 visible-md visible-lg" >
         <div class="text-center index-icons-block">
-            <img src="/images/plane.png" alt="Авиабилеты" width="120" style="padding-top: 20px;">
+            <img src="/images/plane.png" alt="Авиабилеты">
             <span>Авиабилеты</span>
         </div>
         <div class="text-center index-icons-block">
-            <img src="/images/plane.png" alt="Авиабилеты" width="120" style="padding-top: 20px;">
+            <img src="/images/plane.png" alt="Авиабилеты">
             <span>Авиабилеты</span>
         </div>
         <div class="text-center index-icons-block">
-            <img src="/images/plane.png" alt="Авиабилеты" width="120" style="padding-top: 20px;">
+            <img src="/images/plane.png" alt="Авиабилеты">
             <span>Авиабилеты</span>
         </div>
         <div class="text-center index-icons-block">
-            <img src="/images/plane.png" alt="Авиабилеты" width="120" style="padding-top: 20px;">
+            <img src="/images/plane.png" alt="Авиабилеты">
             <span>Авиабилеты</span>
         </div>
         <div class="text-center index-icons-block">
-            <img src="/images/plane.png" alt="Авиабилеты" width="120" style="padding-top: 20px;">
+            <img src="/images/plane.png" alt="Авиабилеты">
             <span>Авиабилеты</span>
         </div>
     </div>
 </div>
 <div class="row" style="margin-top: 20px;">
-    <div class="col-xs-12">
+    <div class="col-xs-12 visible-md visible-lg">
         <div class="text-center index-icons-block">
-            <img src="/images/plane.png" alt="Авиабилеты" width="120" style="padding-top: 20px;">
+            <img src="/images/plane.png" alt="Авиабилеты">
             <span>Авиабилеты</span>
         </div>
         <div class="text-center index-icons-block">
-            <img src="/images/plane.png" alt="Авиабилеты" width="120" style="padding-top: 20px;">
+            <img src="/images/plane.png" alt="Авиабилеты">
             <span>Авиабилеты</span>
         </div>
         <div class="text-center index-icons-block">
-            <img src="/images/plane.png" alt="Авиабилеты" width="120" style="padding-top: 20px;">
+            <img src="/images/plane.png" alt="Авиабилеты">
             <span>Авиабилеты</span>
         </div>
         <div class="text-center index-icons-block">
-            <img src="/images/plane.png" alt="Авиабилеты" width="120" style="padding-top: 20px;">
+            <img src="/images/plane.png" alt="Авиабилеты">
             <span>Авиабилеты</span>
         </div>
         <div class="text-center index-icons-block">
-            <img src="/images/plane.png" alt="Авиабилеты" width="120" style="padding-top: 20px;">
+            <img src="/images/plane.png" alt="Авиабилеты">
             <span>Авиабилеты</span>
         </div>
     </div>
