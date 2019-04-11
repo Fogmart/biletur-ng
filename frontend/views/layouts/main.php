bd797ac01574e95027b7aaba92af74685f5fe0fe
vlad <newsperparser@gmail.com>
2019-04-11 17:09:42 +1000

    верстка главной страницы

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -54,7 +54,41 @@ AppAsset::register($this);
 </div>
 <footer class="footer">
     <div class="container">
-
+        <div class="row">
+            <div class="col-md-3">
+                <ul class="list-unstyled">
+                    <li><a href="#">О компании</a></li>
+                    <li><a href="#">Корпоративное обслуживание</a></li>
+                    <li><a href="#">Партнерам</a></li>
+                    <li><a href="#">Новости</a></li>
+                </ul>
+            </div>
+            <div class="col-md-3">
+                <ul class="list-unstyled">
+                    <li><a href="#">Авиабилеты</a></li>
+                    <li><a href="#">Ж/Д билеты</a></li>
+                    <li><a href="#">Туры</a></li>
+                    <li><a href="#">Круизы</a></li>
+                </ul>
+            </div>
+            <div class="col-md-3">
+                <ul class="list-unstyled">
+                    <li><a href="#">Визы</a></li>
+                    <li><a href="#">Страхование</a></li>
+                    <li><a href="#">Отели</a></li>
+                    <li><a href="#">Экскурсии</a></li>
+                </ul>
+            </div>
+            <div class="col-md-3">
+                <ul class="list-unstyled">
+                    <li><a href="#">Бронирование авто</a></li>
+                    <li><a href="#">Билеты на мероприятия</a></li>
+                </ul>
+            </div>
+        </div>
+        <div class="row hr-del-line inverse">
+            <div class="col-xs-12"></div>
+        </div>
     </div>
 </footer>
 
--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -18,44 +18,44 @@ $this->title = 'Билетур';
             <span>Авиабилеты</span>
         </div>
         <div class="text-center index-icons-block">
-            <img src="/images/plane.png" alt="Авиабилеты">
-            <span>Авиабилеты</span>
+            <img src="/images/plane.png" alt="Ж/Д билеты">
+            <span>Ж/Д билеты</span>
         </div>
         <div class="text-center index-icons-block">
-            <img src="/images/plane.png" alt="Авиабилеты">
-            <span>Авиабилеты</span>
+            <img src="/images/plane.png" alt="Экскурсии">
+            <span>Экскурсии</span>
         </div>
         <div class="text-center index-icons-block">
-            <img src="/images/plane.png" alt="Авиабилеты">
-            <span>Авиабилеты</span>
+            <img src="/images/plane.png" alt="Туры">
+            <span>Туры</span>
         </div>
         <div class="text-center index-icons-block">
-            <img src="/images/plane.png" alt="Авиабилеты">
-            <span>Авиабилеты</span>
+            <img src="/images/plane.png" alt="Отели">
+            <span>Отели</span>
         </div>
     </div>
 </div>
 <div class="row" style="margin-top: 20px;">
     <div class="col-xs-12 visible-md visible-lg">
         <div class="text-center index-icons-block">
-            <img src="/images/plane.png" alt="Авиабилеты">
-            <span>Авиабилеты</span>
+            <img src="/images/plane.png" alt="Визы">
+            <span>Визы</span>
         </div>
         <div class="text-center index-icons-block">
-            <img src="/images/plane.png" alt="Авиабилеты">
-            <span>Авиабилеты</span>
+            <img src="/images/plane.png" alt="Круизы">
+            <span>Круизы</span>
         </div>
         <div class="text-center index-icons-block">
-            <img src="/images/plane.png" alt="Авиабилеты">
-            <span>Авиабилеты</span>
+            <img src="/images/plane.png" alt="Страхование">
+            <span>Страхование</span>
         </div>
         <div class="text-center index-icons-block">
-            <img src="/images/plane.png" alt="Авиабилеты">
-            <span>Авиабилеты</span>
+            <img src="/images/plane.png" alt="Аренда авто">
+            <span>Аренда авто</span>
         </div>
         <div class="text-center index-icons-block">
-            <img src="/images/plane.png" alt="Авиабилеты">
-            <span>Авиабилеты</span>
+            <img src="/images/plane.png" alt="Билеты на мероприятия">
+            <span>Билеты на мероприятия</span>
         </div>
     </div>
 </div>
