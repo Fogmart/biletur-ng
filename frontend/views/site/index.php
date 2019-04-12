5b2d0231b993a26a670f7e2120bd7facca32705d
vlad <newsperparser@gmail.com>
2019-04-12 15:19:12 +1000

    права доступа

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -9,57 +9,44 @@ use frontend\controllers\RailRoadController;
 $this->title = 'Билетур';
 ?>
 <div class="row">
-    <div class="col-xs-12 text-center">
-        <div class="banner-hr-block">
+    <div class="row column-5">
+        <div class="col-xs-12 text-center">
+            <div class="banner-hr-block">
 
+            </div>
         </div>
     </div>
 </div>
-<div class="row" style="margin-top: 20px;">
-    <div class="col-xs-12 visible-md visible-lg">
+<div class="row column-5" style="margin-top: 20px;">
+    <div class="col-md-2">
         <a href="<?= AviaController::getActionUrl(AviaController::ACTION_INDEX) ?>" class="text-center index-icons-block">
             <img src="/images/plane.png" alt="Авиабилеты">
             <span>Авиабилеты</span>
         </a>
+    </div>
+    <div class="col-md-2">
         <a href="<?= RailRoadController::getActionUrl(RailRoadController::ACTION_INDEX) ?>" class="text-center index-icons-block">
             <img src="/images/plane.png" alt="Ж/Д билеты">
             <span>Ж/Д билеты</span>
         </a>
+    </div>
+    <div class="col-md-2">
         <div class="text-center index-icons-block">
             <img src="/images/plane.png" alt="Экскурсии">
             <span>Экскурсии</span>
         </div>
+    </div>
+    <div class="col-md-2">
         <div class="text-center index-icons-block">
             <img src="/images/plane.png" alt="Туры">
             <span>Туры</span>
         </div>
+    </div>
+    <div class="col-md-2">
         <div class="text-center index-icons-block">
             <img src="/images/plane.png" alt="Отели">
             <span>Отели</span>
         </div>
     </div>
 </div>
-<div class="row" style="margin-top: 20px;">
-    <div class="col-xs-12 visible-md visible-lg">
-        <div class="text-center index-icons-block">
-            <img src="/images/plane.png" alt="Визы">
-            <span>Визы</span>
-        </div>
-        <div class="text-center index-icons-block">
-            <img src="/images/plane.png" alt="Круизы">
-            <span>Круизы</span>
-        </div>
-        <div class="text-center index-icons-block">
-            <img src="/images/plane.png" alt="Страхование">
-            <span>Страхование</span>
-        </div>
-        <div class="text-center index-icons-block">
-            <img src="/images/plane.png" alt="Аренда авто">
-            <span>Аренда авто</span>
-        </div>
-        <div class="text-center index-icons-block">
-            <img src="/images/plane.png" alt="Билеты на мероприятия">
-            <span>Билеты на мероприятия</span>
-        </div>
-    </div>
-</div>
+
