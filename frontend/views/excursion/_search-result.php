9d99d8d529c804a6b3f489318bda455b4feecf06
vlad <newsperparser@gmail.com>
2019-05-24 12:53:50 +1000

    кэширование запросов к апи трипстера

--- frontend/views/excursion/_search-result.php
+++ frontend/views/excursion/_search-result.php
@@ -6,50 +6,63 @@
  * @var \common\forms\excursion\SearchForm $form
  */
 
+$numOfCols = 3;
+$rowCount = 0;
+$bootstrapColWidth = 12 / $numOfCols;
 ?>
     <div class="result">
-        <div class="loading-widget" style="display: none;"></div>
-		<?php if (count($form->tags) > 0): ?>
-            <div class="tags">
+    <div class="loading-widget" style="display: none;"></div>
+	<?php if (count($form->tags) > 0): ?>
+        <div class="tags">
+			<?php
+			$active = '';
+			if (empty($form->cityTag)) {
+				$active = ' active';
+			}
+			?>
+            <a href="#" class="tag <?= $active ?>" data-id="">Все экскурсии</a>
+			<?php foreach ($form->tags as $tag): ?>
 				<?php
 				$active = '';
-				if (empty($form->cityTag)) {
+				if ($form->cityTag == $tag->id) {
 					$active = ' active';
 				}
 				?>
-                <a href="#" class="tag <?= $active ?>" data-id="">Все экскурсии</a>
-				<?php foreach ($form->tags as $tag): ?>
-					<?php
-					$active = '';
-					if ($form->cityTag == $tag->id) {
-						$active = ' active';
-					}
-					?>
-                    <a href="#" class="tag <?= $active ?>" data-id="<?= $tag->id ?>"><?= $tag->name ?> [<?= $tag->experience_count ?>]</a>
-				<?php endforeach; ?>
-            </div>
-		<?php endif ?>
+                <a href="#" class="tag <?= $active ?>" data-id="<?= $tag->id ?>"><?= $tag->name ?> [<?= $tag->experience_count ?>]</a>
+			<?php endforeach; ?>
+        </div>
+	<?php endif ?>
 
-        <div class="list">
-			<?php
-			\common\base\helpers\Dump::d($form->result);
-			?>
+    <div class="list">
+        <div class="row">
 			<?php foreach ($form->result as $excursion): ?>
-
+                <div class="col-xs-<?= $bootstrapColWidth ?>">
+					<?= $this->render('_single', ['excursion' => $excursion]) ?>
+                </div>
+				<?php
+				$rowCount++;
+				if ($rowCount % $numOfCols == 0) {
+					echo '</div><div class="row">';
+				}
+				?>
 			<?php endforeach ?>
 
 			<?php if ($form->pageCount > 1): ?>
-                <ul class="pagination pagination-sm">
-					<?php
-					for ($i = 0; $i < $form->pageCount; $i++) {
-						$active = '';
-						if ($form->page == $i + 1) {
-							$active = ' active';
+                <div class="col-xs-12">
+                    <ul class="pagination pagination-sm">
+						<?php
+						for ($i = 0;
+							 $i < $form->pageCount;
+							 $i++) {
+							$active = '';
+							if ($form->page == $i + 1) {
+								$active = ' active';
+							}
+							echo '<li class="' . $active . '"><a href="#" class="page-num" data-num="' . ($i + 1) . '">' . ($i + 1) . '</a></li>';
 						}
-						echo '<li class="' . $active . '"><a href="#" class="page-num" data-num="' . ($i + 1) . '">' . ($i + 1) . '</a></li>';
-					}
-					?>
-                </ul>
+						?>
+                    </ul>
+                </div>
 			<?php endif ?>
         </div>
     </div>
--- frontend/views/excursion/_single.php
+++ frontend/views/excursion/_single.php
@@ -1,4 +1,16 @@
 <?php
 /**
  * @author Исаков Владислав <visakov@biletur.ru>
+ *
+ * @var \common\components\excursion\CommonExcursion $excursion
+ *
  */
+?>
+<div class="excursion-block">
+	<img class="img img-rounded img-responsive" src="<?= $excursion->image ?>">
+	<div class="name"><h3><?= $excursion->name ?></h3></div>
+	<div class="annotation"><?= $excursion->annotation ?></div>
+	<div class="duration"><?= $excursion->duration ?></div>
+	<div class="price"><?= $excursion->price ?></div>
+	<div class="rating"><?= $excursion->rating ?></div>
+</div>
