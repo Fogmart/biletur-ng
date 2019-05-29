354b199940e2148a551501f31c0265bc5e1fcbdf
vlad <newsperparser@gmail.com>
2019-05-30 09:15:26 +1000

    виджет экскурсий

--- frontend/assets/src/js/widgetPlugin.js
+++ frontend/assets/src/js/widgetPlugin.js
@@ -1,17 +1,5 @@
 (function ($) {
-	$.fn.widgetPlugin = function (method) {
-		if (methods[method]) {
-			return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
-		} else if (typeof method === 'object' || !method) {
-			return methods.init.apply(this, arguments);
-		} else {
-			$.error('Метод "' + method + '" не найден в плагине jQuery.widgetPlugin');
-		}
-	};
-
-	// наши публичные методы
 	var methods = {
-		// инициализация плагина
 		init: function (params) {
 			if (window.addEventListener) {
 				window.addEventListener('load', $(this).widgetPlugin('getDocHeight', $(this)), false);
@@ -19,19 +7,24 @@
 				window.attachEvent('onload', $(this).widgetPlugin('sendDocHeightMsg'));
 			}
 		},
-		getDocHeight: function (doc) {
-			doc = document;
-			var body = doc.body, html = doc.documentElement;
-
-			var height = $('.inner-container').height() + 50;
-
-			return height;
+		getDocHeight: function () {
+			return $('.inner-container').height() + 50;
 		},
 		sendDocHeightMsg: function () {
 			setTimeout(function () {
 				var ht = $(this).widgetPlugin('getDocHeight');
 				parent.postMessage(JSON.stringify({'docHeight': ht}), '*');
 			}, 1500);
+		}
+	};
+
+	$.fn.widgetPlugin = function (method) {
+		if (methods[method]) {
+			return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
+		} else if (typeof method === 'object' || !method) {
+			return methods.init.apply(this, arguments);
+		} else {
+			$.error('Метод "' + method + '" не найден в плагине jQuery.widgetPlugin');
 		}
 	};
 })(jQuery);
