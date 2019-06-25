8d7270245cff2901c877fb4d85df9fd17260a152
vlad <newsperparser@gmail.com>
2019-06-25 16:57:35 +1000

    баннер виджет

--- common/modules/banner/widgets/BannerWidget.php
+++ common/modules/banner/widgets/BannerWidget.php
@@ -3,6 +3,8 @@
 use yii\base\Widget;
 use common\modules\banner\models\Banner;
 use yii\db\Expression;
+use yii\caching\TagDependency;
+
 
 /**
  * @author Исаков Владислав <visakov@biletur.ru>
@@ -19,16 +21,22 @@ class BannerWidget extends Widget {
 	 * @author Исаков Владислав <visakov@biletur.ru>
 	 */
 	public function run() {
+		$cacheKey = Yii::$app->cache->buildKey([__METHOD__, $this->zone]);
+
 		/** @var Banner $banner */
-		$banner = Banner::find()
-			->andWhere([Banner::ATTR_ZONE => $this->zone])
-			->andWhere(['>=', Banner::ATTR_BEG_DATE, new Expression('sysdate')])
-			->andWhere(['<=', Banner::ATTR_END_DATE, new Expression('sysdate')])
-			->one();
+		$banner = Yii::$app->cache->get($cacheKey);
+		if (false === $banner) {
+			$banner = Banner::find()
+				->andWhere([Banner::ATTR_ZONE => $this->zone])
+				->andWhere(['>=', Banner::ATTR_BEG_DATE, new Expression('sysdate')])
+				->andWhere(['<=', Banner::ATTR_END_DATE, new Expression('sysdate')])
+				->one();
 
-		$banner->show_count++;
-		$banner->save();
+			$banner->show_count++;
+			$banner->save();
 
+			Yii::$app->cache->set($cacheKey, $banner, null, new TagDependency(['tags' => [Banner::class]]));
+		}
 		return $this->render('banner', ['banner' => $banner]);
 	}
 }
