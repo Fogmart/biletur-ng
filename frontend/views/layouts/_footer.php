ade0ce3821bd14bc2171f0e64fbca97682dec4d3
vlad <newsperparser@gmail.com>
2019-07-09 10:10:26 +1000

    работа старых ссылок на туры

--- frontend/views/layouts/_footer.php
+++ frontend/views/layouts/_footer.php
@@ -47,13 +47,10 @@ use frontend\controllers\SiteController;
             <div class="col-xs-12"></div>
         </div>
         <div class="row">
-            <div class="col-md-3">
-
-            </div>
-            <div class="col-md-3">
-
-            </div>
-            <div class="col-md-3">
+	        <div class="col-md-5">
+		        © 1998-<?= date('Y')?> АО "Приморское агентство авиационных компаний"
+	        </div>
+            <div class="col-md-4">
 
             </div>
             <div class="col-md-3">
