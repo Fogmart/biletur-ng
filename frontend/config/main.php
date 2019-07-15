ab423a9e0233016624fe10aad22803beacc4c3e4
vlad <newsperparser@gmail.com>
2019-07-15 16:18:26 +1000

    разнесение урл-маршрутизации по модулям

--- frontend/config/main.php
+++ frontend/config/main.php
@@ -9,6 +9,75 @@ $params = array_merge(
 	require __DIR__ . '/params-local.php'
 );
 
+$modulesUrlRules = array_merge(
+	require __DIR__ . '/../../common/modules/banner/config/url-rules.php',
+	require __DIR__ . '/../../common/modules/message/config/url-rules.php',
+	require __DIR__ . '/../../common/modules/news/config/url-rules.php',
+	require __DIR__ . '/../../common/modules/order/config/url-rules.php',
+	require __DIR__ . '/../../common/modules/pages/config/url-rules.php',
+	require __DIR__ . '/../../common/modules/profile/config/url-rules.php',
+	require __DIR__ . '/../../common/modules/seo/config/url-rules.php'
+);
+$commonUrlRules = [
+	''                   => 'site/index',
+	'sitemap.xml'        => 'sitemap/default/index',
+	[
+		'pattern' => 'page',
+		'route'   => 'page',
+		'class'   => StaticPageUrlRule::class,
+	],
+	[
+		'pattern' => 'sitemap',
+		'route'   => 'sitemap/default/index',
+		'suffix'  => '.xml'
+	],
+	'site/set-city/<id>' => '/site/set-city/',
+	'thumbs/<path:.*>'   => 'site/thumb',
+
+	'excursion/find-by-name/<q>/<needType>' => 'excursion/find-by-name',
+	'excursion/widget/<city>'               => 'excursion/widget',
+	'excursion/widget/<city>/<needSearch>'  => 'excursion/widget',
+	'excursion/city/<city>'                 => 'excursion/index',
+
+	'tour/search'            => 'tour/index/',
+	'tour/<id>/<src>/<slug>' => 'tour/view/',
+	'tour/<id>/<src>'        => 'tour/view/',
+
+	'login'   => 'site/login',
+	'profile' => 'profile/profile/index',
+
+	'api/remote-query/'                => 'api/remote-query/index',
+	'api/remote-query/invalidate-tag'  => 'api/remote-query/invalidate-tag',
+	'api/remote-query/add-request-log' => 'api/remote-query/add-request-log',
+
+	'<module:[\w-]+>/<controller:[\w-]+>/<action:[\w-]+>/<id:\d+>]' => '<module>/<controller>/<action>',
+	'<controller:\w+>/<action:\w+>/'                                => '<controller>/<action>',
+	'message-widget/<object>/<objectId>/<userName>'                 => 'message/message/widget',
+	'hotels/find-by-name/q/<q>'                                     => 'hotels/find-by-name',
+
+	//Обработка старых ссылок------------------------------------------------------------
+	'Agency'                                                        => 'old-links/agency',//static
+	'Agency/index.asp'                                              => 'old-links/agency',//static
+	'TimeTbl'                                                       => 'old-links/avia',
+	'TimeTbl/pkc/delays.asp'                                        => 'old-links/avia',
+	'TimeTbl/vvo/delays.asp'                                        => 'old-links/avia',
+	'Passenger/vvo/railway_schedule.asp'                            => 'old-links/rail-road',
+	'Agency/Rekvizit.asp'                                           => 'old-links/accounts',//static
+	'Agency/foradvertisers.asp'                                     => 'old-links/advertising',//static
+	'Tourism/tour.asp'                                              => 'old-links/tour',
+	'tourism/tour.asp'                                              => 'old-links/tour',
+	'tourism/hotel/'                                                => 'old-links/hotels',
+	'order/ordPayInfo.asp'                                          => 'old-links/ord-pay-info',//static
+	'BackCall/BackCall.asp'                                         => 'old-links/back-call',//dynamic form
+	'Agency/vacancy.asp'                                            => 'old-links/vacancy',//dynamic
+	'avianet/default.asp'                                           => 'old-links/partners',//static
+	'Agency/structura/index.asp'                                    => 'old-links/agency-struct',//static
+	'Agency/Filials/default.asp'                                    => 'old-links/filials',//dynamic
+	'Agency/Commendations.asp'                                      => 'old-links/commendations',//static
+	'Agency/awards.asp'                                             => 'old-links/awards', //static
+	//---------------------------------------------------------------------------------------
+];
+
 return [
 	'id'                  => 'app-frontend',
 	'name'                => 'Всероссийская сеть Билетур',
@@ -43,65 +112,7 @@ return [
 			'enablePrettyUrl' => true,
 			'showScriptName'  => false,
 			'suffix'          => '/',
-			'rules'           => [
-				''                   => 'site/index',
-				'sitemap.xml'        => 'sitemap/default/index',
-				[
-					'pattern' => 'page',
-					'route'   => 'page',
-					'class'   => StaticPageUrlRule::class,
-				],
-				[
-					'pattern' => 'sitemap',
-					'route'   => 'sitemap/default/index',
-					'suffix'  => '.xml'
-				],
-				'site/set-city/<id>' => '/site/set-city/',
-				'thumbs/<path:.*>'   => 'site/thumb',
-
-				'excursion/find-by-name/<q>/<needType>' => 'excursion/find-by-name',
-				'excursion/widget/<city>'               => 'excursion/widget',
-				'excursion/widget/<city>/<needSearch>'  => 'excursion/widget',
-				'excursion/city/<city>'                 => 'excursion/index',
-
-				'tour/search'            => 'tour/index/',
-				'tour/<id>/<src>/<slug>' => 'tour/view/',
-				'tour/<id>/<src>'        => 'tour/view/',
-
-				'login'   => 'site/login',
-				'profile' => 'profile/profile/index',
-
-				'api/remote-query/'                => 'api/remote-query/index',
-				'api/remote-query/invalidate-tag'  => 'api/remote-query/invalidate-tag',
-				'api/remote-query/add-request-log' => 'api/remote-query/add-request-log',
-
-				'<module:[\w-]+>/<controller:[\w-]+>/<action:[\w-]+>/<id:\d+>]' => '<module>/<controller>/<action>',
-				'<controller:\w+>/<action:\w+>/'                                => '<controller>/<action>',
-				'message-widget/<object>/<objectId>/<userName>'                 => 'message/message/widget',
-				'hotels/find-by-name/q/<q>'                                     => 'hotels/find-by-name',
-
-				//Обработка старых ссылок------------------------------------------------------------
-				'Agency'                                                        => 'old-links/agency',//static
-				'Agency/index.asp'                                              => 'old-links/agency',//static
-				'TimeTbl'                                                       => 'old-links/avia',
-				'TimeTbl/pkc/delays.asp'                                        => 'old-links/avia',
-				'TimeTbl/vvo/delays.asp'                                        => 'old-links/avia',
-				'Passenger/vvo/railway_schedule.asp'                            => 'old-links/rail-road',
-				'Agency/Rekvizit.asp'                                           => 'old-links/accounts',//static
-				'Agency/foradvertisers.asp'                                     => 'old-links/advertising',//static
-				'Tourism/tour.asp'                                              => 'old-links/tour',
-				'tourism/tour.asp'                                              => 'old-links/tour',
-				'tourism/hotel/'                                                => 'old-links/hotels',
-				'order/ordPayInfo.asp'                                          => 'old-links/ord-pay-info',//static
-				'BackCall/BackCall.asp'                                         => 'old-links/back-call',//dynamic form
-				'Agency/vacancy.asp'                                            => 'old-links/vacancy',//dynamic
-				'avianet/default.asp'                                           => 'old-links/partners',//static
-				'Agency/structura/index.asp'                                    => 'old-links/agency-struct',//static
-				'Agency/Filials/default.asp'                                    => 'old-links/filials',//dynamic
-				'Agency/Commendations.asp'                                      => 'old-links/commendations',//static
-				'Agency/awards.asp'                                             => 'old-links/awards', //static
-				//---------------------------------------------------------------------------------------
-			],
+			'rules'           => array_merge($commonUrlRules, $modulesUrlRules)
 		],
 	],
 	'params'              => $params,
